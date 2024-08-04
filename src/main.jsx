318aae25bc7caf4811035603ff7df7103bc75912
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-08-04 04:49:08 +0100

    Add Routes & ?integrate? Routing capability

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Components/Layout.jsx'
+import Home from './Components/Home.jsx'
+import Cart from './Components/Cart.jsx'
+import ErrorPage from './Components/ErrorPage.jsx'
+
+let routes = createBrowserRouter([
+  {
+    path: "/",
+    element: Layout,
+    errorElement: ErrorPage,
+    children: [
+      {
+        index: true,
+        element: Home,
+      },
+      {
+        path: "cart",
+        element: Cart
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
