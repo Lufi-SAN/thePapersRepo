6a9f4e8795d58941a448e93dd566f1b5e1ebf2aa
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-08-18 05:03:56 +0100

    Add navbar responsiveness & move filter logic to Home Route

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,31 +1,28 @@
-import { useOutletContext } from "react-router-dom"
+import { useOutletContext, useSearchParams } from "react-router-dom"
+import Carousel from "./Carousel.jsx"
 
 function Home() {
+    const [componentProductsData] = useOutletContext();
+    const [filterParameter, setFilterParameter] = useSearchParams();
+    console.log(filterParameter)
+
     return (
         <>
+
             <div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
-            <div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div><div>Sup</div>
+            <div className="flex">
+                <div className="flex ">
+
+                </div>
+                <div>
+                    <Carousel />
+                </div>
+            </div>
         </>
     )
 }
 // Grid goes here, it depends on the product array which can be filtered or not, we have the newsletter form & Carousel* & aside for hiring, maybe testimonials
 // and an extra banner; We change the url params here(function to change the url params)
 // Won't be full width; Can do with auto margins + max-width, flex or grid entire thing
+// We'll filter here i guess
 export default Home
