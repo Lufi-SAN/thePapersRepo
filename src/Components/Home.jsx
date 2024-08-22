2ab7a5919322d5eb5b1dceb5efdb67620c8e3060
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-08-22 10:23:15 +0100

    Added to store offers section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,15 +8,26 @@ function Home() {
 
     return (
         <>
+            <div className="md:mx-6">
+                <section className="flex flex-col" aria-label="Store Offers">
+                    <ul className="grid grid-cols-1 grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 order-last lg:order-first">
+                        <li className="p-6 text-center border-b-[1px] border-b-gray-100 lg:border-r-2 lg:border-r-gray-100">
+                            <p className="text-gray-400 mb-2">Download the app</p>
+                            <p className="">Get an exclusive &#8358;1500 off code</p>
+                        </li>
+                        <li className="p-6 text-center border-b-[1px] border-b-gray-100 lg:border-r-2 lg:border-r-gray-100">
+                            <p className="text-gray-400 text-center mb-2">Return when you&apos;re ready</p>
+                            <p className="text-center">60 days of free returns</p>
+                        </li>
+                        <li className="p-6 text-center border-b-[1px] border-b-gray-100">
+                            <p className="text-gray-400 mb-2">Sign up for our newsletter</p>
+                            <p>15% off your first order</p>
+                        </li>
+                    </ul>
+                    <div className="h-[100px] w-full">
 
-            <div>Sup</div>
-            <div className="flex">
-                <div className="flex ">
-
-                </div>
-                <div>
-                    <Carousel />
-                </div>
+                    </div>
+                </section>
             </div>
         </>
     )
@@ -25,4 +36,5 @@ function Home() {
 // and an extra banner; We change the url params here(function to change the url params)
 // Won't be full width; Can do with auto margins + max-width, flex or grid entire thing
 // We'll filter here i guess
+//My tailwind JIT isn't responding, heaven knows why
 export default Home
