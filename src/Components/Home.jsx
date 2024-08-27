f87a5834e766221f1ed3bbe1c5641cf97e372db5
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-08-27 06:24:34 +0100

    Add sections to main; Add toggleable filter bar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,22 @@
+import { useEffect, useRef, useState } from "react";
 import { useOutletContext, useSearchParams } from "react-router-dom"
-import Carousel from "./Carousel.jsx"
 
 function Home() {
     const [componentProductsData] = useOutletContext();
+
+    const [isFilterExpanded, setisFilterExpanded] = useState(false)
+    const filterRef = useRef(null)
+
     const [filterParameter, setFilterParameter] = useSearchParams();
     console.log(filterParameter)
 
+    function showFilterDiv() {
+        setisFilterExpanded((prev) => !prev)
+    }
+
     return (
         <>
-            <div className="md:mx-6">
+            <div className="lg:mx-6">
                 <section className="flex flex-col" aria-label="Store Offers">
                     <ul className="grid grid-cols-1 grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 order-last lg:order-first">
                         <li className="p-6 text-center border-b-[1px] border-b-gray-100 lg:border-r-2 lg:border-r-gray-100">
@@ -24,8 +32,23 @@ function Home() {
                             <p>15% off your first order</p>
                         </li>
                     </ul>
-                    <div className="h-[100px] w-full">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-1 ">
+                        <div className="pt-[96px] p-[48px] w-auto lg:py-[256px] lg:pl-[32px] lg:pr-[48px] bg-gray-50">
+                            <p className="font-bold text-[36px] lg:text-[48px] mb-[32px]">It&apos;s shiny cause it&apos;s good for you</p>
+                            <p className="leading-normal text-gray-600 mb-[32px] text-[18px] lg:text-[20px]">If a ring is a piece of jewellery I&apos;ll be wearing for the rest of my life, a minimum 10k investment isn&apos;t insane in the grand scheme of things</p>
+                            <button type="button" className="text-[18px] py-[16px] px-[48px] text-white bg-primaryLight hover:bg-primary focus:bg-primary font-bold rounded-lg">Shop Jewelry</button>
+                        </div>
+                        <div>
+                            <img src="/TheOneRing_1280.jpg" alt="A ring of great power" srcSet="/TheOneRing_500.jpg 500w, /TheOneRing_768.jpg 768w, /TheOneRing_1280.jpg 1280w" className="h-[100%] object-cover" />
+                        </div>
+                    </div>
+                </section>
 
+                <section aria-labelledby="Product List" className="mt-[64px]">
+                    <h2 id="Product List" className="text-[30px]">Our Products</h2>
+                    <div className="w-full bg-gray-50 mt-[8px] py-[8px] pl-[8px]"><button aria-controls="filterDiv" onClick={showFilterDiv} className="text-primary px-[16px] py-[4px] border border-primary rounded-lg">Sort</button></div>
+                    <div role="Product Filter" id="filterDiv" aria-expanded={isFilterExpanded} className={`${isFilterExpanded ? "hidden" : ""} flex`} ref={filterRef}>
+                        Hello
                     </div>
                 </section>
             </div>
