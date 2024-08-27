adb31c0435dfadf34ec2f83bc05b6e40f01f0054
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-08-27 06:22:04 +0100

    Add accessibility attributes to main navigation components

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -18,12 +18,12 @@ function Layout() {
         <>
             <header className="sticky top-0" role="header">
                 <nav className="flex w-full px-6 py-6 border-b border-b-slate-200 bg-white">
-                    <h1 className={`text-[32px] font-extrabold text-primary font-azeret ${textcolor}`}>the<span className={`inline-block py-1 bg-primary ${textcolor2}`}>papers</span></h1>
-                    <ul className="me-0 ms-auto flex text-slate-500 navul">
+                    <h1 className={`text-[30px] lg:text-[36px] font-extrabold text-primary font-azeret ${textcolor}`} aria-label="Company Logo">the<span className={`inline-block py-1 bg-primary ${textcolor2}`}>papers</span></h1>
+                    <ul className="me-0 ms-auto flex text-slate-500 navul" role="navigation">
                         <li className="flex items-center"><NavLink to="/">About</NavLink></li>
                         <li className="flex items-center"><NavLink to="/">Home</NavLink></li>
                         <li className="flex items-center"><NavLink to="/">GHub</NavLink></li>
-                        <li className="flex items-center gap-x-1"><NavLink to="cart" className="flex items-center h-full"><svg className="h-[16px] h-8 aspect-square" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32" strokeWidth={1.5} stroke="currentColor">
+                        <li className="flex items-center gap-x-1"><NavLink to="cart" className="flex items-center h-full"><svg className="h-8 aspect-square" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32" strokeWidth={1.5} stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                         </svg><p>{cartCounter.length}</p>
                         </NavLink></li>
